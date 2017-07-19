3ab560cdf429130faed492f95424958cbe014d24
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-19 16:33:20 +0100

    SSL

---
+++ Fiver.Security.SSL/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Security.SSL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+    }
+}
---
+++ Fiver.Security.SSL/Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Security.SSL.Controllers
+{
+    //[RequireHttps]
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Movies");
+        }
+    }
+}
